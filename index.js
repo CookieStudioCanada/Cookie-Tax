4d85e163c497a4a0d90b7e76154ff9957a5563ee
CookieStudioCanada <92235638+CookieStudioCanada@users.noreply.github.com>
2021-10-13 12:53:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 let gciEl = document.getElementById("gci-el")
 let gciTaxEl = document.getElementById("gci-tax-el")
 let gciTextEl = document.getElementById("gci-text-el")
+let gciSpaTextEl = document.getElementById("gci-spa-text-el")
+
+let gcTextEl = document.getElementById("gcText-el")
 
 let ld = false
 let spa = false
 
-
 // Calcul revenu data
 let taxEl = document.getElementById("tax-el")
 
+// Calculatrice - Intérêts composés
+let intEl = document.getElementById("placement-el")
+let intTextEl = document.getElementById("placement-text-el")
+
 // function
+
+
 
 function gcButton()
 {
@@ -19,8 +27,20 @@ function gcButton()
     let pbr = document.getElementById("PBR").value
     sum = (pd - pbr) / 2
     console.log(sum)
-    gciEl.textContent = "GCI : " + sum
-    gciTaxEl.textContent = "Montant a payer : " + sum / 2 // comme si le GC etait payable a 50%... 
+    gciEl.textContent = "GCI : " + sum + " $" // possible d'obtenir des espaces ou virgule? 180 000 ou 180,000?
+    gciTaxEl.textContent = "Montant a payer : " + sum / 2 + " $"// comme si le GC etait payable a 50%... 
+
+    // permet de display le block qui est invisible au depart avec la classe hidden.
+    if (spa === true)
+    {
+      gcTextEl.style.display = "block"
+      gciTextEl.style.display = "none" // meilleur methode? 
+      gciSpaTextEl.style.display = "block" 
+    } else {
+      gcTextEl.style.display = "block"
+      gciSpaTextEl.style.display = "none" 
+      gciTextEl.style.display = "block" 
+    }
 }
 
 function revenuButton()
@@ -28,8 +48,21 @@ function revenuButton()
     let revenu = document.getElementById("Income").value
     let depense = document.getElementById("Charge").value
     impot = (revenu - depense)
-    console.log(impot)
     taxEl.textContent = "Impot : " + impot
+}
+
+function placementButton()
+{
+    let placement = document.getElementById("Placement").value
+    let versement = document.getElementById("Versement").value
+    let interet = document.getElementById("Interet").value
+    let annee = document.getElementById("Annee").value
+    
+    initial = ((interet / 100) + 1) * placement 
+    versements = ((interet / 100) + 1) * (versement * 12 * annee)
+    amount = initial + versements // pas le bon calcul. Existe-t-il une fonction avec math? 
+    intEl.textContent = "Placements : " + amount
+    intTextEl.style.display = "block"
 }
 
 // Determine si lien de dependance ou non. Toutefois, ce n'est pas defini ce que le toggle change. C'est un boolean.
@@ -61,6 +94,7 @@ function spaToggle() // ce qui se
 function tabs(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
+    gcTextEl.style.display = "none" // je reset manuellement qu'il n'y ait pas de text Saviez-vous du GC... Ca fonctionne...
   
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -78,3 +112,5 @@ function tabs(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  tabs(Event, 'Menu') // pas certain de comprendre pourquoi Event avec un E majuscule fonctionne, mais tant mieux...
